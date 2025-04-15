bf58ece1c2a58fd8c9dd353e8ede0e4b96cd24eb
Rajnikant <rajnikantb2002@gmail.com>
2025-04-15 07:42:41 +0530

    structure is done

---
+++ flutter_application_1/lib/constants.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const Color kBackgroundColor = Color(0xFFFFFFFF);
+const Color kCardColor = Color(0xFFFFFFFF);
+const Color kPrimaryColor = Color(0xFF4169E1);
+const Color kInfoIconColor = Color(0xFF666666);
+const Color kBorderColor = Color(0xFFE0E0E0);
