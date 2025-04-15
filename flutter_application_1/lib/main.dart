0d9d033c14a64f0740e22827c11e9788c0f8a360
Rajnikant <rajnikantb2002@gmail.com>
2025-04-15 08:08:38 +0530

    color icons add

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -28,27 +28,205 @@ class FreightScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Card(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
-            elevation: 0,
-            color: kCardColor,
-            child: Padding(
-              padding: const EdgeInsets.all(20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Text("Search the best Freight Rates",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold)),
-                      Spacer(),
-                      Column(
+        child: Container(
+          width: double.infinity,
+          height: double.infinity,
+          padding: EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            color: Color(0xFF0139FF).withOpacity(0.1),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Header Row
+              Container(
+                width: 800,
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text("Search the best Freight Rates",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold)),
+                    Spacer(),
+                    Container(
+                      width: 90,
+                      height: 35,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(19),
+                        color: Color(0xFF0139FF).withOpacity(0.1),
+                        border: Border.all(
+                          color: Color(0xFF0139FF),
+                          width: 1,
+                        ),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          Container(
+                          Text(
+                            "History",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Color(0xFF0139FF),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+
+              SizedBox(height: 20),
+
+              // White Container with Form
+              Expanded(
+                child: Container(
+                  width: double.infinity,
+                  padding: EdgeInsets.all(20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        // Origin and Destination
+                        Row(
+                          children: [
+                            Expanded(child: customField("Origin")),
+                            SizedBox(width: 16),
+                            Expanded(child: customField("Destination")),
+                          ],
+                        ),
+                        SizedBox(height: 8),
+                        Row(
+                          children: [
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  SizedBox(
+                                    width: 24,
+                                    height: 24,
+                                    child: Checkbox(
+                                      value: false,
+                                      onChanged: (_) {},
+                                      activeColor: Color(0xFF0139FF),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(4),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 8),
+                                  Text(
+                                    "Include nearby origin ports",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.black87,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Expanded(
+                              child: Row(
+                                children: [
+                                  SizedBox(
+                                    width: 24,
+                                    height: 24,
+                                    child: Checkbox(
+                                      value: false,
+                                      onChanged: (_) {},
+                                      activeColor: Color(0xFF0139FF),
+                                      shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(4),
+                                      ),
+                                    ),
+                                  ),
+                                  SizedBox(width: 8),
+                                  Text(
+                                    "Include nearby destination ports",
+                                    style: TextStyle(
+                                      fontSize: 12,
+                                      color: Colors.black87,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 12),
+                        Row(
+                          children: [
+                            Expanded(child: customField("Commodity")),
+                            SizedBox(width: 16),
+                            Expanded(
+                                child:
+                                    customField("Cut Off Date", isDate: true)),
+                          ],
+                        ),
+
+                        SizedBox(height: 20),
+
+                        Text("Shipment Type:",
+                            style: TextStyle(color: Colors.black)),
+                        Row(
+                          children: [
+                            customCheckbox("FCL", true),
+                            SizedBox(width: 10),
+                            customCheckbox("LCL", false),
+                          ],
+                        ),
+
+                        SizedBox(height: 12),
+
+                        Row(
+                          children: [
+                            Expanded(child: dropdownContainer()),
+                            SizedBox(width: 16),
+                            Expanded(child: customField("No of Boxes")),
+                            SizedBox(width: 16),
+                            Expanded(child: customField("Weight (Kg)")),
+                          ],
+                        ),
+
+                        SizedBox(height: 8),
+                        Row(
+                          children: [
+                            Icon(Icons.info_outline,
+                                size: 16, color: Colors.black54),
+                            SizedBox(width: 4),
+                            Flexible(
+                              child: Text(
+                                "To obtain accurate rate for spot rate with guaranteed space and booking, please ensure your container count and weight per container is accurate.",
+                                style: TextStyle(
+                                    fontSize: 12, color: Colors.black54),
+                              ),
+                            ),
+                          ],
+                        ),
+
+                        SizedBox(height: 20),
+
+                        Text("Container Internal Dimensions :",
+                            style: TextStyle(color: Colors.black)),
+                        SizedBox(height: 10),
+                        Text(
+                          "Length: 39.46 ft\nWidth: 7.70 ft\nHeight: 7.84 ft",
+                          style: TextStyle(color: Colors.black),
+                        ),
+
+                        SizedBox(height: 24),
+                        Align(
+                          alignment: Alignment.bottomRight,
+                          child: Container(
                             width: 90,
                             height: 35,
                             decoration: BoxDecoration(
@@ -62,8 +240,11 @@ class FreightScreen extends StatelessWidget {
                             child: Row(
                               mainAxisAlignment: MainAxisAlignment.center,
                               children: [
+                                Icon(Icons.search,
+                                    color: Color(0xFF0139FF), size: 20),
+                                SizedBox(width: 4),
                                 Text(
-                                  "History",
+                                  "Search",
                                   style: TextStyle(
                                     fontSize: 12,
                                     color: Color(0xFF0139FF),
@@ -72,138 +253,13 @@ class FreightScreen extends StatelessWidget {
                               ],
                             ),
                           ),
-                        ],
-                      ),
-                    ],
-                  ),
-
-                  SizedBox(height: 20),
-
-                  // Origin and Destination
-                  Row(
-                    children: [
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Origin',
-                            prefixIcon:
-                                Icon(Icons.location_on, color: kPrimaryColor),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                              borderSide: BorderSide(color: kBorderColor),
-                            ),
-                          ),
-                        ),
-                      ),
-                      SizedBox(width: 16),
-                      Expanded(
-                        child: TextField(
-                          decoration: InputDecoration(
-                            labelText: 'Destination',
-                            prefixIcon:
-                                Icon(Icons.location_on, color: kPrimaryColor),
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(8),
-                              borderSide: BorderSide(color: kBorderColor),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ],
-                  ),
-                  SizedBox(height: 12),
-                  Row(
-                    children: [
-                      Expanded(child: customField("Commodity")),
-                      SizedBox(width: 16),
-                      Expanded(
-                          child: customField("Cut Off Date", isDate: true)),
-                    ],
-                  ),
-
-                  SizedBox(height: 20),
-
-                  Text("Shipment Type:"),
-                  Row(
-                    children: [
-                      customCheckbox("FCL", true),
-                      SizedBox(width: 10),
-                      customCheckbox("LCL", false),
-                    ],
-                  ),
-
-                  SizedBox(height: 12),
-
-                  // Container Size
-                  Row(
-                    children: [
-                      Expanded(child: dropdownContainer()),
-                      SizedBox(width: 16),
-                      Expanded(child: customField("No of Boxes")),
-                      SizedBox(width: 16),
-                      Expanded(child: customField("Weight (Kg)")),
-                    ],
-                  ),
-
-                  SizedBox(height: 8),
-                  Row(
-                    children: [
-                      Icon(Icons.info_outline, size: 16, color: kInfoIconColor),
-                      SizedBox(width: 4),
-                      Flexible(
-                        child: Text(
-                          "To obtain accurate rate for spot rate with guaranteed space and booking, please ensure your container count and weight per container is accurate.",
-                          style: TextStyle(fontSize: 12, color: kInfoIconColor),
-                        ),
-                      ),
-                    ],
-                  ),
-
-                  SizedBox(height: 20),
-
-                  Text("Container Internal Dimensions :"),
-                  SizedBox(height: 10),
-                  Text("Length: 39.46 ft\nWidth: 7.70 ft\nHeight: 7.84 ft"),
-
-                  SizedBox(height: 24),
-                  Align(
-                    alignment: Alignment.bottomRight,
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Container(
-                          width: 90,
-                          height: 35,
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(19),
-                            color: Color(0xFF0139FF).withOpacity(0.1),
-                            border: Border.all(
-                              color: Color(0xFF0139FF),
-                              width: 1,
-                            ),
-                          ),
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Icon(Icons.search,
-                                  color: Color(0xFF0139FF), size: 20),
-                              SizedBox(width: 4),
-                              Text(
-                                "Search",
-                                style: TextStyle(
-                                  fontSize: 12,
-                                  color: Color(0xFF0139FF),
-                                ),
-                              ),
-                            ],
-                          ),
                         ),
                       ],
                     ),
-                  )
-                ],
+                  ),
+                ),
               ),
-            ),
+            ],
           ),
         ),
       ),
@@ -214,6 +270,9 @@ class FreightScreen extends StatelessWidget {
     return TextFormField(
       decoration: InputDecoration(
         labelText: label,
+        prefixIcon: (label == "Origin" || label == "Destination")
+            ? Icon(Icons.location_on, color: Colors.black)
+            : null,
         suffixIcon: isDate ? Icon(Icons.calendar_today_outlined) : null,
         filled: true,
         fillColor: Colors.white,
